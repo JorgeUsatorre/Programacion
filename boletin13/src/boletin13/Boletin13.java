5893e4f9783b1347c817bc1c46de236c596753c0
JorgeUsatorre <usatorre1210@gmail.com>
2022-12-13 09:56:09 +0100

    Boletin

---
+++ boletin13/src/boletin13/Boletin13.java
@@ -0,0 +1,15 @@
+
+package boletin13;
+
+
+public class Boletin13 {
+
+
+    public static void main(String[] args) {
+        
+        Academia obx = new Academia(2,2018);
+        System.out.println("Nashe"+obx.toString());
+        
+    }
+    
+}
